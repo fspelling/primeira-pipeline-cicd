451fb01cfc08e91a7e865a1d4490ab48f499ffc9
Fernando Spelling <41520406+fspelling@users.noreply.github.com>
2025-12-23 09:25:33 -0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -7,9 +7,8 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo - v3")
+	fmt.Fprintf(w, "Aplicacao exemplo - v4")
 }
 
 func main() {
